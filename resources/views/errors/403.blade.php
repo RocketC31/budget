8907e6a5bddf27645d28eb6e323a48e552d8590d
RocketDev <rocket@mailo.com>
2022-04-08 18:31:03 +0200

    Remove unused blade template

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,7 +1,3 @@
-@extends('layout')
-
-@section('body')
-    <div class="wrapper wrapper--narrow my-3">
-        <h2 class="text-center">403</h2>
-    </div>
-@endsection
+<div style="text-align: center;" class="wrapper wrapper--narrow my-3">
+    <h2 class="text-center">403</h2>
+</div>
