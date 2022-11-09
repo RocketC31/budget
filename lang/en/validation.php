5362d03bf747f444761b567eeefe6efdfd4b24f0
Sebastiaan Speck <shem.speck@gmail.com>
2022-11-09 10:31:48 +0100

    Add missing translation

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -11,5 +11,6 @@ return [
     'forbidden' => 'Not permitted to use this resource',
     'budget_like_this_exist' => 'A budget like this already exists',
     'please_upload_csv' => 'Please upload a CSV',
-    'exists' => "This user doesn't exist"
+    'exists' => "This user doesn't exist",
+    'date_format' => 'The date does not match the format :format.',
 ];
--- lang/nl/validation.php
+++ lang/nl/validation.php
@@ -10,5 +10,6 @@ return [
     'forbidden' => 'Niet toegestaan om deze resource te gebruiken',
     'budget_like_this_exist' => 'Een budget zaosl deze bestaat al',
     'please_upload_csv' => 'Upload alsjeblieft een CSV',
-    'exists' => 'Deze gebruiker bestaat niet'
+    'exists' => 'Deze gebruiker bestaat niet',
+    'date_format' => 'Datum komt niet overeen met het formaat :format',
 ];
