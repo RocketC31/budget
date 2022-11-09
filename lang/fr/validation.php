aa56e48172e220f382520b17f02945902f23a031
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2022-11-09 10:40:25 +0100

    Add missing translation (#62)

--- lang/fr/validation.php
+++ lang/fr/validation.php
@@ -10,5 +10,6 @@ return [
     'forbidden' => 'Non autorisé à utiliser cette ressource',
     'budget_like_this_exist' => 'Un tel budget existe déjà',
     'please_upload_csv' => 'Merci d\'envoyer un CSV',
-    'exists' => "Cet utilisateur n'existe pas"
+    'exists' => "Cet utilisateur n'existe pas",
+    'date_format' => 'Date ne correspond pas au format :format.',
 ];
