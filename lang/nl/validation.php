b0f450b8435e0a65822d12c43521c96baa5f1aab
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2022-11-09 10:40:51 +0100

    Update Dutch translations (#64)

--- lang/nl/validation.php
+++ lang/nl/validation.php
@@ -1,14 +1,15 @@
 <?php
 
 return [
-    'required' => 'Kan niet leeg zijn',
+    'required' => 'Het veld is verplicht',
     'confirmed' => 'Komt niet overeen',
+    
     'max' => [
-        'string' => 'Kan niet langer zijn dan :max karakters'
+        'string' => 'Mag niet groter zijn dan :max karakters',
     ],
 
     'forbidden' => 'Niet toegestaan om deze resource te gebruiken',
-    'budget_like_this_exist' => 'Een budget zaosl deze bestaat al',
+    'budget_like_this_exist' => 'Een budget zoals deze bestaat al',
     'please_upload_csv' => 'Upload alsjeblieft een CSV',
     'exists' => 'Deze gebruiker bestaat niet',
     'date_format' => 'Datum komt niet overeen met het formaat :format',
