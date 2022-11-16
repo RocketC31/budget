e2187c55eac829d8ecbc20e24cc397717eb1259d
Sebastiaan Speck <shem.speck@gmail.com>
2022-11-16 17:31:09 +0100

    Fix styling for Tag when using Light theme

--- resources/js/Components/Partials/Tag.vue
+++ resources/js/Components/Partials/Tag.vue
@@ -1,5 +1,11 @@
 <template>
-    <span :style="'background:#'+tag.color"><i class="fas fa-tag fa-xs"></i> {{ tag.name }}</span>
+    <div class="flex items-center dark:text-gray-500">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+            <path d="M9.16667 2.5L16.6667 10C17.0911 10.4745 17.0911 11.1922 16.6667 11.6667L11.6667 16.6667C11.1922 17.0911 10.4745 17.0911 10 16.6667L2.5 9.16667V5.83333C2.5 3.99238 3.99238 2.5 5.83333 2.5H9.16667" :stroke="'#'+ tag.color" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
+            <circle cx="7.50004" cy="7.49967" r="1.66667" :stroke="'#'+ tag.color" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></circle>
+        </svg>
+        <p class="ml-1">{{ tag.name }}</p>
+    </div>
 </template>
 
 <script setup>
@@ -7,12 +13,3 @@
         tag: Object
     });
 </script>
-<style scoped>
-    span {
-        border-radius: 5px;
-        color: #FFF;
-        padding: 5px 10px;
-        font-size: 14px;
-        font-weight: 600;
-    }
-</style>
