8824bb442a73e7d91e4b8b5fc0364abcb6ba4cc5
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2022-11-17 10:07:13 +0100

    Use default Laravel error pages (#74)

--- resources/views/errors/403.blade.php
+++
@@ -1,3 +0,0 @@
-<div style="text-align: center;" class="wrapper wrapper--narrow my-3">
-    <h2 class="text-center">403</h2>
-</div>
--- resources/views/errors/404.blade.php
+++
@@ -1,3 +0,0 @@
-<div style="text-align: center;" class="wrapper wrapper--narrow my-3">
-    <h2 class="text-center">404</h2>
-</div>
