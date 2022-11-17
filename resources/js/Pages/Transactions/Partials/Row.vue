df0ab44ac27d9c5fc648cd7fe2ee19dbec231594
Sebastiaan Speck <shem.speck@gmail.com>
2022-11-17 21:03:30 +0100

    Use Partials to prevent duplicate code

--- resources/js/Pages/Transactions/Partials/Row.vue
+++ resources/js/Pages/Transactions/Partials/Row.vue
@@ -9,15 +9,7 @@
             </div>
         </td>
         <td class="px-3 text-center">
-            <div class="flex items-center dark:text-gray-500">
-                <template v-if="transaction.tag">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                        <path d="M9.16667 2.5L16.6667 10C17.0911 10.4745 17.0911 11.1922 16.6667 11.6667L11.6667 16.6667C11.1922 17.0911 10.4745 17.0911 10 16.6667L2.5 9.16667V5.83333C2.5 3.99238 3.99238 2.5 5.83333 2.5H9.16667" :stroke="'#'+ transaction.tag.color" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
-                        <circle cx="7.50004" cy="7.49967" r="1.66667" :stroke="'#'+ transaction.tag.color" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></circle>
-                    </svg>
-                    <p class="ml-1">{{ transaction.tag.name }}</p>
-                </template>
-            </div>
+            <Tag :tag="transaction.tag"></Tag>
         </td>
         <td class="px-3 w-10 text-center dark:text-gray-500">
             <i v-if="transaction.recurring_id" class="fas fa-recycle"></i>
@@ -47,6 +39,7 @@
     import { formatDate } from '@/tools';
     import { trans } from "matice";
     import { Link } from "@inertiajs/inertia-vue3";
+    import Tag from '@/Components/Partials/Tag';
     import { Inertia } from '@inertiajs/inertia'
 
     const props = defineProps({
