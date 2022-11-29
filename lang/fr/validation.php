1a21e135b8881ce3455f2ff8cd3056f6aedc3932
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2022-11-29 05:03:50 +0100

    Fix validation messages for Imports/Create (#97)

--- lang/fr/validation.php
+++ lang/fr/validation.php
@@ -10,7 +10,7 @@ return [
 
     'forbidden' => 'Non autorisé à utiliser cette ressource',
     'budget_like_this_exist' => 'Un tel budget existe déjà',
-    'please_upload_csv' => 'Merci d\'envoyer un CSV',
     'exists' => "Cet utilisateur n'existe pas",
     'date_format' => 'Date ne correspond pas au format :format.',
+    'mimes' => 'Le fichier doit être de type : :values.'
 ];
--- lang/nl/validation.php
+++ lang/nl/validation.php
@@ -10,7 +10,7 @@ return [
 
     'forbidden' => 'Niet toegestaan om deze resource te gebruiken',
     'budget_like_this_exist' => 'Een budget zoals deze bestaat al',
-    'please_upload_csv' => 'Upload alsjeblieft een CSV',
     'exists' => 'Deze gebruiker bestaat niet',
     'date_format' => 'Datum komt niet overeen met het formaat :format',
+    'mimes' => "Dit moet een bestand zijn van het bestandstype :values.",
 ];
